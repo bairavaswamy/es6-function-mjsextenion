08b7c649967e88dfb26ee2e44a5fc0cd5cc70fc6
bairavaswamy <bairavahoney@gmail.com>
2023-12-19 07:11:18 +0000

    es6-new-set

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,7 @@
+const myArray = ['camel', 265, true, '5.6']
+const bulb = {watts: 10, type: 'LED'}
+const multiplyByFour = function (num) {
+  return num * 4
+}
+
+export {myArray, bulb, multiplyByFour}
